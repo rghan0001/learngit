cfbbd2ebe5a263ac2b0408fc94857b8e5f456cc2
mzhang <zhangming@m.eiwdv.gyyx.cn>
2019-08-19 15:21:50 +0800

    Test lua update 1

--- TestLua.lua
+++ TestLua.lua
@@ -7,4 +7,15 @@ end
 function GiftBoxMgr:OnCreate()
 end
 
+function GiftBoxMgr:GetGiftArrayByBoxId(giftBoxId)
+    local giftArray = Array.New()
+    local allData = DataTableMgr:GetInstance():GetOneTable(TableEnum.GiftBox)
+    for i, data in pairs(allData) do
+        if data.giftBoxId == giftBoxId then
+            giftArray:Add(data)
+        end
+    end
+    return giftArray
+end
+
 return GiftBoxMgr
